e3171c1ddc6dacfd842a2e8c4031b946ccf91e55
poopy2121 <ggg1111g1g1g@gmail.com>
2024-01-19 21:19:51 +0100

    start

---
+++ script.js
@@ -0,0 +1,21 @@
+const kelvin = 293;
+const celsius = kelvin - 273;
+var fahrenheit = celsius * (9/5) + 32
+var roundedfahrenheit = Math.floor(fahrenheit);
+var usercelsius = document.getElementById("celsius").value;
+
+
+
+
+
+
+
+function celsiustofahrenheit() {
+
+    
+    
+}
+
+
+
+console.log(roundedfahrenheit); 
